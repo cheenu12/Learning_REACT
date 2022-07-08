98834dff91289cc6f9dc7a98944e04887f423577
chirag batheja <99245027+cheenu12@users.noreply.github.com>
2022-07-08 13:40:42 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>hello</h1>
+    <p>my name is chirag batheja</p>
+  </div>,
+
+  document.getElementById("root")
+);
